1e5a2aa47a5e244332f0b8849b59ad3ff85b166a
Justin Dalva Wicent <jdwicent.2022@scis.smu.edu.sg>
2024-08-30 17:51:55 +0800

    remove secret key to env

--- backend/TapSOS/TapSOS/settings.py
+++ backend/TapSOS/TapSOS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from config.env import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ec^sh@yb99=efb@%_u^l#3ey*7s67)z)yjyo^p@40=-nyj6b^w'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
