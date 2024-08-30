229762f4689667d499a92734ed84cf9bd569bce0
Justin Dalva Wicent <jdwicent.2022@scis.smu.edu.sg>
2024-08-30 19:59:40 +0800

    turn llm into service

--- backend/TapSOS/TapSOS/services/llm.py
+++ backend/TapSOS/TapSOS/services/llm.py
@@ -6,39 +6,43 @@ project_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(project_dir)
 
 from openai import OpenAI
-from dotenv import load_dotenv
-
 from config.env import OPENAI_API_KEY
 
+class LLMService:
+    def __init__(self, api_key):
+        self.client = OpenAI(api_key=api_key)
 
-load_dotenv()
+    def generate_card(self, input_data):
+        # edit instructions
+        openai_instructions_card_generation = '''
+    Purpose: Create emergency cards for non-verbal individuals (PWD) to communicate vital information quickly.
+    Input: Personal details, including medical conditions and allergies.
+    Output: Start with list, then { } for each (similar to JSON format):
+        title: Short and clear
+        content: A brief, direct message indicating that the person is currently facing the described emergency and specifying the appropriate help to call 
+        if condition seemed not very severe, can call the person's emergency contact (family member). 
+                Should begin with stating the medical conditions related to the incident IF its related.
+    Additional card with 'crime related' as title, separated by , for each card (similar to JSON format). 
+    By the end of each sentence, add "you can view my personal medication info here".
+    Based on the context, specify the appropriate emergency number in Singapore.
+        '''
 
-def generate_sentence(input):
-    client = OpenAI(
-        api_key=OPENAI_API_KEY,
-    )
+        input_text = " ".join(input_data)
 
-    #edit instructions
-    openai_instructions = '''
-    reply with 'hello, how are you?'
-    '''
+        response = self.client.chat.completions.create(
+            model='gpt-4o',
+            messages=[
+                {"role": "system", "content": f'{openai_instructions_card_generation}'},
+                {"role": "user", "content": f'{input_text}'}
+            ]
+        )
 
-    input_text = " ".join(input)
+        generated_card = response.choices[0].message.content
 
-    response = client.chat.completions.create(
-        model='gpt-4o',
-        messages=[
-            {"role": "system", "content": f'{openai_instructions}'},
-            {"role": "user", "content": f'{input_text}'}
-        ]
-    )
-
-    generated_sentence = response.choices[0].message.content
-
-    print(generated_sentence)
-    return generated_sentence
+        print(generated_card)
+        return generated_card
 
 if __name__ == "__main__":
+    llm_service = LLMService(api_key=OPENAI_API_KEY)
     # EXAMPLE ONLY
-    result = generate_sentence("hi") 
-    # print(result)
+    result = llm_service.generate_card("medical conditions: Mutism. Allergies: peanuts. Emergency contact number: 85135459") 
