ca628d59d070d82522cb1034eef166818b424a11
Justin Dalva Wicent <jdwicent.2022@scis.smu.edu.sg>
2024-08-30 16:18:05 +0800

    change directory

--- backend/TapSOS/TapSOS/config/env.py
+++ backend/TapSOS/TapSOS/config/env.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 
 project_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(project_dir)
 
 prefix_to_clear = "SOS_"
 keys_to_clear = [key for key in os.environ if key.startswith(prefix_to_clear)]
--- backend/TapSOS/TapSOS/services/llm.py
+++ backend/TapSOS/TapSOS/services/llm.py
@@ -1,21 +1,29 @@
 # move somewhere later
+import sys
+import os
+
+project_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(project_dir)
+
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
+
 from config.env import OPENAI_API_KEY
+
 
 load_dotenv()
 
-def generate_sentence(word_list):
+def generate_sentence(input):
     client = OpenAI(
         api_key=OPENAI_API_KEY,
     )
 
+    #edit instructions
     openai_instructions = '''
     reply with 'hello, how are you?'
     '''
 
-    input_text = " ".join(word_list)
+    input_text = " ".join(input)
 
     response = client.chat.completions.create(
         model='gpt-4o',
@@ -27,6 +35,7 @@ def generate_sentence(word_list):
 
     generated_sentence = response.choices[0].message.content
 
+    print(generated_sentence)
     return generated_sentence
 
 if __name__ == "__main__":
